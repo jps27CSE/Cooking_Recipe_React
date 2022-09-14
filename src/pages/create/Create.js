780db6989652489385a4dcc1f3843acf2696d511
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-09-14 23:15:50 +0600

    post request

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import { useFetch } from "../../hooks/useFetch";
 import "./Create.css";
 
 export default function Create() {
@@ -9,8 +10,19 @@ export default function Create() {
   const [ingredients, setIngredients] = useState([]);
   const ingredientInput = useRef(null);
 
+  const { postData, data, error } = useFetch(
+    "http://localhost:8000/recipes",
+    "POST"
+  );
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    postData({
+      title,
+      ingredients,
+      method,
+      cookingTime: cookingTime + " minutes",
+    });
   };
 
   const handleAdd = (e) => {
