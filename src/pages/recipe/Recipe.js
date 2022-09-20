8cbd7337a3662912fc15ad523c559565d8bcac19
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-09-21 00:31:58 +0600

    real time update

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -20,11 +20,10 @@ export default function Recipe() {
   useEffect(() => {
     setIsPending(true);
 
-    projectFirestore
+    const unsub = projectFirestore
       .collection("recipes")
       .doc(id)
-      .get()
-      .then((doc) => {
+      .onSnapshot((doc) => {
         if (doc.exists) {
           setIsPending(false);
           setRecipe(doc.data());
@@ -33,11 +32,13 @@ export default function Recipe() {
           setError("Could not find that recipe");
         }
       });
+
+    return () => unsub();
   }, [id]);
 
   const handleClick = () => {
     projectFirestore.collection("recipes").doc(id).update({
-      title: "Something Different Test",
+      title: "Update",
     });
   };
 
